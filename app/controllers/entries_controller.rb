f778210e2ea0842edf71d6f9ae74a15fe60cb977
Casey Kolderup <casey@kolderup.org>
2014-06-06 20:10:34 -0400

    first pass at entry controller stuff

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,33 @@
 class EntriesController < ApplicationController
+
+  def create
+    @parent = Entry.find(params[:parent])
+    @entry = @parent.children.new entry_params
+
+    if @entry.save then
+      redirect_to @entry
+    else
+      render :new
+    end
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
+  def destroy
+    @entry = current_user.entries.find(params[:id])
+    @parent = @entry.parent
+
+    @entry.destroy
+    flash.notice 'Entry deleted.'
+
+    redirect_to @parent
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions
+  resources :entries
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
