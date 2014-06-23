f90c80bd4967068e75d5878a137632961e2fe5f8
Casey Kolderup <casey@kolderup.org>
2014-06-23 08:12:42 -0400

    add user#update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:notice] = "Details updated!"
+      redirect_back_or_default(user_path(@user))
+    else
+      render "edit"
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
