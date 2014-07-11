458ddaceca61a287ae6e5bb488b4d9e1dc5f7545
Casey Kolderup <casey@kolderup.org>
2014-07-11 00:31:37 -0400

    eradicate paths

--- db/migrate/20140530004935_create_paths.rb
+++
@@ -1,8 +0,0 @@
-class CreatePaths < ActiveRecord::Migration
-  def change
-    create_table :paths do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20140530035116_add_story_to_paths.rb
+++
@@ -1,5 +0,0 @@
-class AddStoryToPaths < ActiveRecord::Migration
-  def change
-    add_column :paths, :story, :reference
-  end
-end
