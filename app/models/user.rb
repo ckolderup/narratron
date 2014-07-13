c44da99453990267a82c1c3444089130cb6ad7af
Casey Kolderup <casey@kolderup.org>
2014-07-12 21:27:05 -0400

    email login should be case-insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,24 +17,29 @@ class User < ActiveRecord::Base
 
   attr_accessor :password
   before_save :encrypt_password
+  before_save :downcase_email
 
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_presence_of :display_name
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, case_sensitive: false
 
   has_many :entries, dependent: :destroy
   has_many :favorites
   has_many :stories, through: :entry
 
   def self.authenticate(email, password)
-    user = find_by_email(email)
+    user = find_by_email(email.downcase)
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
       user
     else
       nil
     end
+  end
+
+  def downcase_email
+    self.email = self.email.downcase
   end
 
   def encrypt_password
