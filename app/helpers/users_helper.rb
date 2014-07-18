659aa15595520a930f6e35659dfc0632b4e98ee1
Casey Kolderup <casey@kolderup.org>
2014-07-18 00:13:15 -0400

    Label user form button correctly depending on state

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def user_form_submit_button_text(user = nil)
+    if current_user
+      "Update#{' your' if current_user == user} profile"
+    else
+      "Sign up"
+    end
+  end
 end
