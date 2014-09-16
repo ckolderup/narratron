27ca28aed941f33c3e418d589fdae6dc0d1f71f0
Casey Kolderup <casey@kolderup.org>
2014-09-15 20:13:58 -0400

    don't break archive page when an empty story gets closed

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,4 +3,14 @@ module HomeHelper
     "#{story.day.present? ? 'Public story for' : 'Private story created'} \
      #{(story.day.present? ? story.day.date : story.created_at).strftime('%A %B %e')}"
   end
+
+  def last_author_display(leaf)
+   if leaf.user.nil?
+     leaf.story.thanks || 'Anonymous'
+   elsif leaf.user == current_user
+     'you'
+   else
+     leaf.user.display_name
+   end
+  end
 end
