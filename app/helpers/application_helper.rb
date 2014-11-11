1c430b5af02bd1ec7d9609a7a8271e8b9c1f65bc
Casey Kolderup <casey@kolderup.org>
2014-11-10 22:47:49 -0500

    add favicon & touch icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def apple_touch_icon(size)
+    link = ''
+    link << '<link rel=\"apple-touch-icon\" '
+    link << "sizes=\"#{xbyx(size)}\"" if size.present?
+    link << "href=\"/images/apple-touch-icon"
+    link << "-#{xbyx(size)}" if size.present?
+    link << ".png\" />"
+    link.html_safe
+  end
+
+  def xbyx(x)
+    "#{x}x#{x}"
+  end
 end
