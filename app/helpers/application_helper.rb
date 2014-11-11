6721ea9fd3b6012c8d67ccc148801979d5b12158
Casey Kolderup <casey@kolderup.org>
2014-11-10 23:00:47 -0500

    use asset pipeline for images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,17 @@
 module ApplicationHelper
-  def apple_touch_icon(size)
+  def apple_touch_icon_tag(size)
     link = ''
     link << '<link rel=\"apple-touch-icon\" '
     link << "sizes=\"#{xbyx(size)}\"" if size.present?
-    link << "href=\"/images/apple-touch-icon"
-    link << "-#{xbyx(size)}" if size.present?
-    link << ".png\" />"
+    link << "href=\"#{apple_touch_icon_path(size)}\" />"
     link.html_safe
+  end
+
+  def apple_touch_icon_path(size)
+    filename = "apple-touch-icon"
+    filename << "-#{xbyx(size)}" if size.present?
+    filename << ".png"
+    image_path(filename)
   end
 
   def xbyx(x)
